030a09729f4db5adaec8b09fb0300c3a3abaec91
cgrad01 <cgrad01@gmail.com>
2016-04-28 14:29:55 -0400

    add #get_count_hash and #count_letters

--- word.rb
+++ word.rb
@@ -1,11 +1,31 @@
 class Word
 
-  attr_accessor :string, :letters, :count
+  attr_reader :string
+  attr_accessor :count_hash
 
   def initialize(args = {})
     @string = args[:string]
-    @letters = self.string.split("")
-    @count = 0
+    @count_hash = self.get_count_hash
+    self.count_letters
+  end
+
+  def get_count_hash()
+    result = {}
+    letters = self.string.split("").uniq
+    letters.each do |letter|
+      result[letter] = 0
+    end
+    return result
+  end
+
+  def count_letters()
+    self.count_hash.each_key do |key|
+      self.string.chars.each do |char|
+        if key == char
+          self.count_hash[key] +=1
+        end
+      end
+    end
   end
 
 end
