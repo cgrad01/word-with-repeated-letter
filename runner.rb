f62aa12760aec5270a7eb7086ba3eeaf1f7372b2
cgrad01 <cgrad01@gmail.com>
2016-04-28 17:40:45 -0400

    add run method

--- runner.rb
+++ runner.rb
@@ -1,11 +1,15 @@
 require_relative 'word'
 require_relative 'parser'
 
-puts "After you have copied the input file into this folder, please input the name of the file (with extension) here:"
-filename = $stdin.gets.chomp
+def run()
+  puts "After you have copied the input file into this folder, please input the name of the file (with extension) here:"
+  filename = $stdin.gets.chomp
 
-parser = Parser.new(filename)
-strings = parser.get_words(parser.normalize)
-words = Word.make_words(strings)
-words.sort!{|a,b| b.max_repeat <=> a.max_repeat}
-puts words.first.string
+  parser = Parser.new(filename)
+  strings = parser.get_words(parser.normalize)
+  words = Word.make_words(strings)
+  words.sort!{|a,b| b.max_repeat <=> a.max_repeat}
+  puts words.first.string
+end
+
+run
