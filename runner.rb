f50a31b9eb88f27432cdfa43c9f77d3580678951
cgrad01 <cgrad01@gmail.com>
2016-04-28 17:39:21 -0400

    edit runner

--- runner.rb
+++ runner.rb
@@ -3,13 +3,9 @@ require_relative 'parser'
 
 puts "After you have copied the input file into this folder, please input the name of the file (with extension) here:"
 filename = $stdin.gets.chomp
+
 parser = Parser.new(filename)
 strings = parser.get_words(parser.normalize)
-
 words = Word.make_words(strings)
-
-sorted = words.sort do |a, b|
-  b.max_repeat <=> a.max_repeat
-end
-
-puts sorted.first.string
+words.sort!{|a,b| b.max_repeat <=> a.max_repeat}
+puts words.first.string
